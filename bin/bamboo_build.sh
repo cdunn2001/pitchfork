b2bc30dd492161355e410ce5bbeab37aef3374fd
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-03-22 18:32:46 -0700

    update

---
+++ bin/bamboo_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+type module >& /dev/null \
+|| . /mnt/software/Modules/current/init/bash
+
+module load gcc/4.9.2
+module load graphviz/2.28.0
+module load ccache/3.2.3
+
+cat > .settings.mk << EOF
+DISTFILE   = ${HOME}/.distfiles
+CCACHE_DIR = /mnt/secondary/Share/tmp/bamboo.mobs.ccachedir
+EOF
+
+make -j third-party
+
+# vim: ft=sh
