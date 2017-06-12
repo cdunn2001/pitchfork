813c66383a82b4f03f0f742aa59e4ef16eb359ea
Christopher Dunn <cdunn2001@gmail.com>
2017-06-12 13:13:57 -0500

    settings.cdmac.mk

---
+++ settings.cdmac.mk
@@ -0,0 +1,18 @@
+# mkdir -p ~/pf/var/pkg
+# xcode-select --install
+FC                    = gfortran-5
+PREFIX                = /Users/cdunn2001/pf
+# export PYTHONUSERBASE=${PREFIX}
+USE_CCACHE            = aye
+HAVE_CCACHE           = /usr/local/bin/ccache
+HAVE_CMAKE            = /use/local/bin/cmake
+HAVE_HDF5             = /usr/local
+HAVE_BOOST            = /usr/local
+HAVE_PYTHON           = /usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/bin/python
+HAVE_ZLIB             = /usr
+HAVE_LIBBZIP2         = /usr
+CXX                   = clang++
+CC                    = clang
+falcon_kit_REPO	= ~/repo/gh/FALCON-integrate/FALCON
+pypeFLOW_REPO	= ~/repo/gh/FALCON-integrate/pypeFLOW
+export VERBOSE=1
